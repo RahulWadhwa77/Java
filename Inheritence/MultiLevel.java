8237527e0f3b47972144bca97fe47c9aec7ac01e
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-01-11 21:47:41 +0530

    Update MultiLevel.java

--- Inheritence/MultiLevel.java
+++ Inheritence/MultiLevel.java
@@ -1,1 +1,26 @@
 
+package revision;
+
+class ClassA{
+	void methodA() {
+		System.out.println("I am a method of ClassA");
+	}
+}
+
+class ClassB extends ClassA{
+	void methodB() {
+		System.out.println("I am a method of ClassB");
+	}
+}
+
+class ClassC extends ClassB{
+	public static void main(String[] args) {
+		ClassC objectC = new ClassC();
+		objectC.methodA();
+		objectC.methodB();
+		objectC.methodC();
+	}
+	void methodC() {
+		System.out.println("I am a method of ClassC");
+	}
+}
