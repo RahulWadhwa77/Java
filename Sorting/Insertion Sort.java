1751bc18187d68c09c897fcf1b66d5840df1cc06
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-01-19 22:02:08 +0530

    Update Insertion Sort.java

--- Sorting/Insertion Sort.java
+++ Sorting/Insertion Sort.java
@@ -7,15 +7,15 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] arr = {18, 11, 9, 3, 5};
 		System.out.println("Array after each swap: ");
-		for (int i = 1; i < arr.length; ++i) { 
-            		int key = arr[i];   		//element that is being compared
-            		int j = i - 1; 
-  
-            		while (j >= 0 && arr[j] > key) { 
-                		arr[j + 1] = arr[j]; 
-                		j = j - 1; 
-            		} 
-            		arr[j + 1] = key; 
+		for(int i = 1;i < arr.length;i++){
+            int temp = arr[i];
+            int j = i;
+            while(j > 0 && arr[j-1]>temp){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = temp;
+        
             
             		System.out.println(Arrays.toString(arr));
         	} 
