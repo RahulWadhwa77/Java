2251fee3394a6aa5fe3cf77fda0e1ceefb9bb1dc
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-01-19 22:03:51 +0530

    Update Insertion Sort.java

--- Sorting/Insertion Sort.java
+++ Sorting/Insertion Sort.java
@@ -7,18 +7,14 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		int[] arr = {18, 11, 9, 3, 5};
 		
-		
 		for(int i = 1;i < arr.length;i++){
-            int temp = arr[i];
-            int j = i;
-            while(j > 0 && arr[j-1]>temp){
-                arr[j] = arr[j-1];
-                j--;
-            }
-            arr[j] = temp;
-        
-            
-            		
+            	   int temp = arr[i];
+                   int j = i;
+                   while(j > 0 && arr[j-1]>temp){
+                	arr[j] = arr[j-1];
+                	j--;
+            	   }
+            	   arr[j] = temp;	
         	} 
 		System.out.println("\nFinal Sorted array: ");
 		System.out.println(Arrays.toString(arr));
