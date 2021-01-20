34e3f42b70410ee05386827d61c5d3242e2c7f73
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-01-20 20:43:16 +0530

    Update Pattern1.java

--- PatternLogics/Pattern1.java
+++ PatternLogics/Pattern1.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n = sc.nextInt();       
+        int i = 1;
+        while(i <= n){
+            int sp = 1;
+            while(sp <= n - i){
+                System.out.print(" ");
+                sp++;
+            }
+            int j = n - (i - 1);
+            while(j <= n + (i -1)){
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+	}
+
+}
 
