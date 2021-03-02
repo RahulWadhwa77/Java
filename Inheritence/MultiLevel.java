4cfb5b5c4528968965b3e8302d7088bad95bb900
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-03-02 19:42:07 +0530

    Update MultiLevel.java

--- Inheritence/MultiLevel.java
+++ Inheritence/MultiLevel.java
@@ -14,7 +14,9 @@ class ClassB extends ClassA{
 }
 
 class ClassC extends ClassB{
+	
 	public static void main(String[] args) {
+		
 		ClassC objectC = new ClassC();
 		objectC.methodA();
 		objectC.methodB();
