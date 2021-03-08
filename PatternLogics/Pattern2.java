8a5c46c4716852e96565e669375f8dc98d083484
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-03-08 22:35:02 +0530

    Update Pattern2.java

--- PatternLogics/Pattern2.java
+++ PatternLogics/Pattern2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
+		
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
