e65745efe2d84e2775e5e153f40881b149174701
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-03-15 23:49:11 +0530

    Update Pattern4.java

--- PatternLogics/Pattern4.java
+++ PatternLogics/Pattern4.java
@@ -6,8 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 	
 		Scanner sc=new Scanner(System.in);
+		
         	int n=sc.nextInt();
         	int i,j,p=1;
+		
         	for(i=1;i<=n;i++){
             		for(j=1;j<=i;j++){
                 		if(i%2!=0){
