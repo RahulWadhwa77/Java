240e79c83ac59a13af7cd717cfc1ed5ef2f201fe
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-04-05 10:27:06 +0530

    Update Selection Sort.java

--- Sorting/Selection Sort.java
+++ Sorting/Selection Sort.java
@@ -1,9 +1,8 @@
 package revision;
 
-class SelectionSort {
-   
+class SelectionSort {   
    public static void main(String args[]){
-     
+
       int array[] = {10, 25, 20, 63, 96, 57};   //unsorted int type array.
       int size = array.length;
 
