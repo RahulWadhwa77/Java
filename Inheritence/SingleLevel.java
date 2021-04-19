ee911f023318e36f0527e970447089ab62c41e1b
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-04-19 22:01:42 +0530

    Update SingleLevel.java

--- Inheritence/SingleLevel.java
+++ Inheritence/SingleLevel.java
@@ -1,16 +1,13 @@
 package revision;
 
-//ClassA is the parent class.
-
+//ClassA is the parent class
 class ClassA{
-	
 	void methodA() {
 		System.out.println("I am a method of Class A");
 	}
 }
 
 //ClassB is the child class of parent ClassA.
-
 class ClassB extends ClassA {				//use of keyword extends.
 	void methodB() {
 		System.out.println("I am a method of Class B");
