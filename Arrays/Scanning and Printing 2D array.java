f096f26e491dfe730724663a8dad78e58dcda618
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-05-09 22:27:42 +0530

    Update Scanning and Printing 2D array.java

--- Arrays/Scanning and Printing 2D array.java
+++ Arrays/Scanning and Printing 2D array.java
@@ -3,7 +3,6 @@ package revision;
 import java.util.*;
 
 public class ArrayExample {
-	
     public static void main(String[] args) {
 
         String[][] arrS = new String[2][3];				//String array of 2 rows and 3 columns
