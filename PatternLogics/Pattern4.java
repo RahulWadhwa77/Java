a17019f846d7df7f2d9ba32374f25f436645a303
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-05-14 09:31:30 +0530

    Update Pattern4.java

--- PatternLogics/Pattern4.java
+++ PatternLogics/Pattern4.java
@@ -1,7 +1,9 @@
 //image is under imgs directory with same name i.e Pattern4
+
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
 	
 		Scanner sc=new Scanner(System.in);
