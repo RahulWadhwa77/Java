2a3640edfe1528df554181b4cfb5be123b9cd171
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-05-22 22:06:12 +0530

    Update Insertion Sort.java

--- Sorting/Insertion Sort.java
+++ Sorting/Insertion Sort.java
@@ -7,7 +7,6 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		
 		int[] arr = {18, 11, 9, 3, 5};
-		
 		for(int i = 1;i < arr.length;i++){
             	   int temp = arr[i];
                    int j = i;
