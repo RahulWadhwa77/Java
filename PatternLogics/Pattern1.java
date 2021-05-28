be5e700a230ab7377dfa4a53a5a8437a873fe1c3
RahulWadhwa77 <rahul1500.cse18@chitkara.edu.in>
2021-05-28 22:28:08 +0530

    Update Pattern1.java

--- PatternLogics/Pattern1.java
+++ PatternLogics/Pattern1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Solution {
 	
 	public static void main(String[] args) {
-		
         	Scanner sc = new Scanner(System.in);
 		
         	System.out.println("Enter number of rows:");
